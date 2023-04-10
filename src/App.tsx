e2ac7291c3d58c58ea172a5306bbc9027891e390
bruno <brunoqueiroz612@gmail.com>
2023-04-10 08:49:31 -0300

    Add Header component globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
