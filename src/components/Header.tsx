fe8cb615569edf443ce10f6e1a028c57cc23cb6e
bruno <brunoqueiroz612@gmail.com>
2023-04-10 08:55:18 -0300

    Add icon to logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GiRollingBomb as BombLogoIcon } from "react-icons/gi";
 
 const Header = () => {
   return (
     <header className="p-4">
-      <Link to="/" className="text-4xl font-bold text-red-600">
-        Bomb
+      <Link to="/" className="flex text-4xl font-bold text-red-600">
+        B<BombLogoIcon className="text-white" />
+        mb
       </Link>
     </header>
   );
