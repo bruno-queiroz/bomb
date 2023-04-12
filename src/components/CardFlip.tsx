968d1f13b08a34edc62b0fe061e823dee39dd07f
bruno <brunoqueiroz612@gmail.com>
2023-04-12 09:00:48 -0300

    Change CardFlip styles

--- src/components/CardFlip.tsx
+++ src/components/CardFlip.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
+import {
+  GiRollingBomb as BombLogoIcon,
+  GiGoldBar as GoldIcon,
+} from "react-icons/gi";
 
 const CardFlip = () => {
   const [isRotated, setIsRotated] = useState(false);
   return (
     <button
-      className="w-[100px] h-[100px] perspective"
+      className="w-[120px] h-[120px] perspective rounded-lg"
       onClick={() => setIsRotated(!isRotated)}
     >
       <div
-        className={`h-full w-full relative transition duration-300 transform-style ${
+        className={`h-full w-full relative transition duration-300 transform-style rounded-lg ${
           isRotated && "rotate-y"
         }`}
       >
-        <div className="absolute h-full w-full back-face bg-red-500">front</div>
-        <div className="absolute h-full w-full back-face bg-blue-500 rotate-y">
-          back
+        <div className="grid place-items-center absolute h-full w-full back-face rounded-lg bg-gray-950">
+          <div className="flex items-center font-bold text-lg text-red-600">
+            B<BombLogoIcon className="text-white" />
+            mb
+          </div>
+        </div>
+        <div className="grid place-items-center absolute h-full w-full back-face rounded-lg bg-green-500 rotate-y">
+          <div className="text-4xl text-amber-400">
+            <GoldIcon />
+          </div>
         </div>
       </div>
     </button>
