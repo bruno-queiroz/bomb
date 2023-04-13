1fb111c8a1833df9ed82cd266891e0e0e62be4a5
bruno <brunoqueiroz612@gmail.com>
2023-04-13 08:51:56 -0300

    Rotate card by changing deck state

--- src/components/CardFlip.tsx
+++ src/components/CardFlip.tsx
@@ -3,17 +3,27 @@ import {
   GiRollingBomb as BombLogoIcon,
   GiGoldBar as GoldIcon,
 } from "react-icons/gi";
+import { Deck } from "../pages/Bomb";
+import { useBombStore } from "../store/store";
 
-const CardFlip = () => {
-  const [isRotated, setIsRotated] = useState(false);
+const CardFlip = ({ isBomb, isCardRotated, cardIndex }: Deck) => {
+  const deck = useBombStore((state) => state.deck);
+  const setDeck = useBombStore((state) => state.setDeck);
+
+  const rotateCard = () => {
+    const updatedDeck = [...deck];
+    updatedDeck[cardIndex] = { ...updatedDeck[cardIndex], isCardRotated: true };
+    setDeck(updatedDeck);
+  };
+
   return (
     <button
       className="w-[120px] h-[120px] perspective rounded-lg"
-      onClick={() => setIsRotated(!isRotated)}
+      onClick={rotateCard}
     >
       <div
         className={`h-full w-full relative transition duration-300 transform-style rounded-lg ${
-          isRotated && "rotate-y"
+          isCardRotated && "rotate-y"
         }`}
       >
         <div className="grid place-items-center absolute h-full w-full back-face rounded-lg bg-gray-950">
