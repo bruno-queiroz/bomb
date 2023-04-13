d593d37f05e4f03106798d6919e6c7c707f0f2c4
bruno <brunoqueiroz612@gmail.com>
2023-04-13 15:37:51 -0300

    Add playerMoves state

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,10 +3,15 @@ import { Deck } from "../pages/Bomb";
 
 interface BombState {
   deck: Deck[];
+  playerMoves: number;
+  incrementPlayerMoves: () => void;
   setDeck: (newDeck: Deck[]) => void;
 }
 
 export const useBombStore = create<BombState>()((set) => ({
   deck: [],
+  playerMoves: 0,
+  incrementPlayerMoves: () =>
+    set((state) => ({ playerMoves: state.playerMoves + 1 })),
   setDeck: (newDeck) => set((state) => ({ deck: newDeck })),
 }));
