b66a868ea6abac34c1ff9f4e3bef5b5f9d10a380
bruno <brunoqueiroz612@gmail.com>
2023-04-14 15:31:23 -0300

    Add didPlayerWin state

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,18 +5,22 @@ interface BombState {
   deck: Deck[];
   playerMoves: number;
   isEndMatchModalOpen: boolean;
+  didPlayerWin: boolean;
   setDeck: (newDeck: Deck[]) => void;
   incrementPlayerMoves: () => void;
   setIsEndMatchModalOpen: (state: boolean) => void;
+  setDidPlayerWin: (boolean: boolean) => void;
 }
 
 export const useBombStore = create<BombState>()((set) => ({
   deck: [],
   playerMoves: 0,
   isEndMatchModalOpen: false,
+  didPlayerWin: false,
   setDeck: (newDeck) => set((state) => ({ deck: newDeck })),
   incrementPlayerMoves: () =>
     set((state) => ({ playerMoves: state.playerMoves + 1 })),
   setIsEndMatchModalOpen: (boolean) =>
     set((state) => ({ isEndMatchModalOpen: boolean })),
+  setDidPlayerWin: (boolean) => set((state) => ({ didPlayerWin: boolean })),
 }));
