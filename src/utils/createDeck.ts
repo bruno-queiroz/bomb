a79db21dcb5e445f56c4d603620c6686fb7bd9cf
bruno <brunoqueiroz612@gmail.com>
2023-04-14 08:24:17 -0300

    Fix bug subtracting 1 to match index

--- src/utils/createDeck.ts
+++ src/utils/createDeck.ts
@@ -2,15 +2,13 @@ import { generateRandomNumbers } from "./generateRandomNumbers";
 
 export const createDeck = (cardsAmount: number) => {
   const randomNumbers = generateRandomNumbers(3);
-
   const deck = Array.from({ length: cardsAmount }, (_, index) => ({
     isBomb: false,
     cardIndex: index,
     isCardRotated: false,
   }));
-
   randomNumbers.forEach((randomNumber) => {
-    deck[randomNumber] = { ...deck[randomNumber], isBomb: true };
+    deck[randomNumber - 1] = { ...deck[randomNumber - 1], isBomb: true };
   });
 
   return deck;
