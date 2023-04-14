e6d296827cf746b9e19bb059eda45f219e433f6e
bruno <brunoqueiroz612@gmail.com>
2023-04-14 15:30:03 -0300

    Add isEndMatchModalopen state

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,14 +4,19 @@ import { Deck } from "../pages/Bomb";
 interface BombState {
   deck: Deck[];
   playerMoves: number;
-  incrementPlayerMoves: () => void;
+  isEndMatchModalOpen: boolean;
   setDeck: (newDeck: Deck[]) => void;
+  incrementPlayerMoves: () => void;
+  setIsEndMatchModalOpen: (state: boolean) => void;
 }
 
 export const useBombStore = create<BombState>()((set) => ({
   deck: [],
   playerMoves: 0,
+  isEndMatchModalOpen: false,
+  setDeck: (newDeck) => set((state) => ({ deck: newDeck })),
   incrementPlayerMoves: () =>
     set((state) => ({ playerMoves: state.playerMoves + 1 })),
-  setDeck: (newDeck) => set((state) => ({ deck: newDeck })),
+  setIsEndMatchModalOpen: (boolean) =>
+    set((state) => ({ isEndMatchModalOpen: boolean })),
 }));
