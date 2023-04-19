1127d7d24810ba782de2eb66b50499848751e44a
bruno <brunoqueiroz612@gmail.com>
2023-04-19 08:24:42 -0300

    Call generateRandomNumbers including new parameter

--- src/utils/createDeck.ts
+++ src/utils/createDeck.ts
@@ -1,7 +1,11 @@
 import { generateRandomNumbers } from "./generateRandomNumbers";
+import { getGameModeValues } from "./getGameModeValues";
 
 export const createDeck = (cardsAmount: number, numberOfBombs: number) => {
-  const randomNumbers = generateRandomNumbers(numberOfBombs);
+  const randomNumbers = generateRandomNumbers(
+    numberOfBombs,
+    getGameModeValues()?.mode || 12
+  );
   const deck = Array.from({ length: cardsAmount }, (_, index) => ({
     isBomb: false,
     cardIndex: index,
