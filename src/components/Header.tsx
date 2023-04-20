125aca399d0668ea0ec2c53a0014b69003d0c967
bruno <brunoqueiroz612@gmail.com>
2023-04-20 07:17:59 -0300

    Show golds amount

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,30 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { GiRollingBomb as BombLogoIcon } from "react-icons/gi";
+import {
+  GiRollingBomb as BombLogoIcon,
+  GiGoldBar as GoldIcon,
+} from "react-icons/gi";
+import { useBombStore } from "../store/store";
 
 const Header = () => {
+  const golds = useBombStore((state) => state.golds);
   return (
     <header className="p-4">
-      <Link
-        to="/"
-        className="flex text-4xl font-bold text-red-600 w-[max-content]"
-      >
-        B<BombLogoIcon className="text-white" />
-        mb
-      </Link>
+      <div className="flex justify-between items-center">
+        <Link
+          to="/"
+          className="flex text-4xl font-bold text-red-600 w-[max-content]"
+        >
+          B<BombLogoIcon className="text-white" />
+          mb
+        </Link>
+
+        <div className="flex gap-1">
+          <GoldIcon className="text-3xl text-amber-500" />
+          <div className="grid place-items-center text-white rounded bg-gray-800 min-w-[50px]">
+            {golds}
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
