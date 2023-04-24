bb46065cc0770c7f16913f43d8d704394fb6df80
bruno <brunoqueiroz612@gmail.com>
2023-04-24 07:19:02 -0300

    Get gold item out of the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,27 +8,28 @@ import { useBombStore } from "../store/store";
 const Header = () => {
   const golds = useBombStore((state) => state.golds);
   return (
-    <header className="p-4">
-      <div className="flex justify-between items-center">
-        <Link
-          to="/"
-          className="flex text-4xl font-bold text-red-600 w-[max-content]"
-        >
-          B<BombLogoIcon className="text-white" />
-          mb
-        </Link>
-
-        <div className="flex gap-1">
-          <GoldIcon className="text-3xl text-amber-500" />
-          <div
-            className="grid place-items-center text-white rounded bg-gray-800 min-w-[50px]"
-            id="gold-item"
+    <>
+      <header className="fixed p-4 right-0 left-0">
+        <div className="flex justify-between items-center">
+          <Link
+            to="/"
+            className="flex text-4xl font-bold text-red-600 w-[max-content]"
           >
-            {golds}
-          </div>
+            B<BombLogoIcon className="text-white" />
+            mb
+          </Link>
+        </div>
+      </header>
+      <div className="flex gap-1 fixed z-10 right-4 top-5">
+        <GoldIcon className="text-3xl text-amber-500" />
+        <div
+          className="grid place-items-center text-white rounded bg-gray-800 min-w-[50px]"
+          id="gold-item"
+        >
+          {golds}
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
