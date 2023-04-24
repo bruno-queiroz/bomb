c26110a61bbf2222ef4fb1c120bcaba20485d5a4
bruno <brunoqueiroz612@gmail.com>
2023-04-24 16:02:55 -0300

    Add animation in the end of the match

--- src/components/EndMatchModal.tsx
+++ src/components/EndMatchModal.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-import { IoClose as CloseIcon } from "react-icons/io5";
 import { useBombStore } from "../store/store";
 import { Link } from "react-router-dom";
 import { flipAllCardsDown } from "../utils/flipAllCardsDown";
 import { getGameModeValues } from "../utils/getGameModeValues";
+import { GiGoldBar as GoldIcon } from "react-icons/gi";
 
 const EndMatchModal = () => {
   const isEndMatchModalOpen = useBombStore(
@@ -13,6 +13,8 @@ const EndMatchModal = () => {
   const didPlayerWin = useBombStore((state) => state.didPlayerWin);
   const resetMatch = useBombStore((state) => state.resetMatch);
   const setDeck = useBombStore((state) => state.setDeck);
+  const earnedGoldRef = useRef<HTMLDivElement>(null);
+  const [floatGoldIconTopPosition, setFloatGoldIconTopPosition] = useState(0);
 
   const CARD_FLIP_ANIMATION_TIME = 300;
 
@@ -23,51 +25,91 @@ const EndMatchModal = () => {
     }, CARD_FLIP_ANIMATION_TIME);
   };
 
+  useEffect(() => {
+    const root: HTMLDivElement | null = document.querySelector(":root");
+    const goldCounter = document.querySelector("#gold-counter");
+    const goldCounterLocation = goldCounter?.getBoundingClientRect();
+    const earnedGoldLocation = earnedGoldRef.current?.getBoundingClientRect();
+
+    const earnedGoldTopLocation = earnedGoldLocation!.top;
+    const goldCounterLeftLocation = goldCounterLocation?.left;
+    const goldCounterTopLocation = goldCounterLocation?.top;
+    const middleOfTheBody = root?.getBoundingClientRect().width! / 2;
+
+    const middleOfGoldCounterHorizontally = goldCounterLocation?.width! / 2;
+
+    setFloatGoldIconTopPosition(earnedGoldTopLocation);
+
+    root!.style.setProperty(
+      "--y-location",
+      `-${earnedGoldTopLocation! - goldCounterTopLocation!}px`
+    );
+    root!.style.setProperty(
+      "--x-location",
+      `${
+        goldCounterLeftLocation! -
+        middleOfTheBody +
+        middleOfGoldCounterHorizontally
+      }px`
+    );
+  }, [isEndMatchModalOpen]);
+
   return (
-    <div
-      className={`flex justify-center fixed z-10 top-0 bottom-0 text-white left-0 right-0 bg-[rgba(0,0,0,0.5)] ${
-        !isEndMatchModalOpen && "hidden"
-      }`}
-    >
-      <section className="flex flex-col gap-2 max-w-[600px] w-full absolute top-20 z-10 p-4 rounded bg-gray-800">
-        <h2 className="text-white text-center text-4xl text-semibold mb-10">
-          {didPlayerWin
-            ? "Congrats you were a lucky guy this time"
-            : "You caught the bomb this time"}
-        </h2>
+    <>
+      <div
+        style={{
+          top: `${floatGoldIconTopPosition}px`,
+          display: isEndMatchModalOpen ? "flex" : "none",
+        }}
+        className="flex justify-center fixed z-40 w-[100%] carry-gold-up"
+      >
+        <GoldIcon className="text-amber-500 text-4xl carry-gold-right" />
+      </div>
+      <div
+        className={`flex justify-center fixed z-20 top-0 bottom-0 text-white left-0 right-0 bg-[rgba(0,0,0,0.5)] ${
+          !isEndMatchModalOpen && "hidden"
+        }`}
+      >
+        <section className="flex flex-col gap-2 max-w-[600px] w-full absolute top-20 z-10 p-4 rounded bg-gray-800">
+          <h2 className="text-white text-center text-4xl text-semibold mb-10">
+            {didPlayerWin
+              ? "Congrats you were a lucky guy this time"
+              : "You caught the bomb this time"}
+          </h2>
 
-        <div className="flex justify-center mb-4 text-lg">
-          {didPlayerWin ? (
-            <div>
-              You Earned:{" "}
-              <span className="text-amber-500 font-semibold">
-                {getGameModeValues()?.win} golds
-              </span>
-            </div>
-          ) : (
-            <div>
-              You Lost:{" "}
-              <span className="text-red-500 font-semibold">
-                {getGameModeValues()?.loss} golds
-              </span>
-            </div>
-          )}
-        </div>
+          <div className="flex justify-center mb-4 text-lg">
+            {didPlayerWin ? (
+              <div ref={earnedGoldRef}>
+                You Earned:{" "}
+                <span className="text-amber-500 font-semibold">
+                  {getGameModeValues()?.win} golds
+                </span>
+              </div>
+            ) : (
+              <div ref={earnedGoldRef}>
+                You Lost:{" "}
+                <span className="text-red-500 font-semibold">
+                  {getGameModeValues()?.loss} golds
+                </span>
+              </div>
+            )}
+          </div>
 
-        <button
-          className="bg-green-600 py-3 rounded font-semibold text-lg"
-          onClick={playAgain}
-        >
-          Play again
-        </button>
-        <Link
-          to="/game-mode"
-          className="bg-gray-950 text-center py-3 rounded font-semibold"
-        >
-          Change Game Mode
-        </Link>
-      </section>
-    </div>
+          <button
+            className="bg-green-600 py-3 rounded font-semibold text-lg"
+            onClick={playAgain}
+          >
+            Play again
+          </button>
+          <Link
+            to="/game-mode"
+            className="bg-gray-950 text-center py-3 rounded font-semibold"
+          >
+            Change Game Mode
+          </Link>
+        </section>
+      </div>
+    </>
   );
 };
 
