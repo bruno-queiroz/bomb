8df85c0081fb3e1d2e10d32c1eb759549e109f92
bruno <brunoqueiroz612@gmail.com>
2023-08-27 12:39:51 -0300

    Add tests to generateRandomNumber utility function

---
+++ __tests__/utils/generateRandomNumber.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { generateRandomNumber } from "../../src/utils/generateRandomNumber";
+
+describe("Testing generateRandomNumber utility function", () => {
+  it("Should not return a value higher than the limit", () => {
+    const limit1 = 12;
+    const limit2 = 6;
+    const limit3 = 2;
+
+    const randomNumber1 = generateRandomNumber(limit1);
+    const randomNumber2 = generateRandomNumber(limit2);
+    const randomNumber3 = generateRandomNumber(limit3);
+
+    expect(randomNumber1).toBeLessThanOrEqual(limit1);
+    expect(randomNumber2).toBeLessThanOrEqual(limit2);
+    expect(randomNumber3).toBeLessThanOrEqual(limit3);
+  });
+});
