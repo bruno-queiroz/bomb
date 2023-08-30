fccbc46354cda9be1b9329b01d7d76deb6cd544f
bruno <brunoqueiroz612@gmail.com>
2023-08-30 11:16:50 -0300

    Create gameModesMock data

---
+++ __tests__/mocks/gameMode.ts
@@ -0,0 +1,18 @@
+export const gameModesMock = [
+  {
+    mode: 12,
+    bombs: 3,
+    golds: 9,
+    moves: 3,
+    win: 25,
+    loss: 35,
+  },
+  {
+    mode: 20,
+    bombs: 5,
+    golds: 15,
+    moves: 5,
+    win: 30,
+    loss: 40,
+  },
+];
