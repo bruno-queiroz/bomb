1e22adb7c08ea6e91ca71727f2ae91ea39c4ff95
bruno <brunoqueiroz612@gmail.com>
2023-08-31 11:30:04 -0300

    Add dependency injection of the randomNumbers

--- src/utils/createDeck.ts
+++ src/utils/createDeck.ts
@@ -1,16 +1,10 @@
-import { generateRandomNumbers } from "./generateRandomNumbers";
-import { getGameModeValues } from "./getGameModeValues";
-
-export const createDeck = (cardsAmount: number, numberOfBombs: number) => {
-  const randomNumbers = generateRandomNumbers(
-    numberOfBombs,
-    getGameModeValues(window.location.search)?.mode || 12
-  );
+export const createDeck = (randomNumbers: number[], cardsAmount: number) => {
   const deck = Array.from({ length: cardsAmount }, (_, index) => ({
     isBomb: false,
     cardIndex: index,
     isCardRotated: false,
   }));
+
   randomNumbers.forEach((randomNumber) => {
     deck[randomNumber - 1] = { ...deck[randomNumber - 1], isBomb: true };
   });
