c519d4811c1b4bcfa0c5c4c53071576c4d605732
bruno <brunoqueiroz612@gmail.com>
2023-09-01 11:23:48 -0300

    Add createCard parameter

--- src/utils/createDeck.ts
+++ src/utils/createDeck.ts
@@ -1,9 +1,13 @@
-export const createDeck = (randomNumbers: number[], cardsAmount: number) => {
-  const deck = Array.from({ length: cardsAmount }, (_, index) => ({
-    isBomb: false,
-    cardIndex: index,
-    isCardRotated: false,
-  }));
+import { Card } from "../models/card";
+
+export const createDeck = (
+  randomNumbers: number[],
+  cardsAmount: number,
+  createCard: (index: number) => Card
+) => {
+  const deck = Array.from({ length: cardsAmount }, (_, index) =>
+    createCard(index)
+  );
 
   randomNumbers.forEach((randomNumber) => {
     deck[randomNumber - 1] = { ...deck[randomNumber - 1], isBomb: true };
