3834ba87d47472e5d7e127a0c5026ea5a7f36849
bruno <brunoqueiroz612@gmail.com>
2023-09-20 11:12:26 -0300

    Add new e2e tests to gameMode

--- cypress/e2e/gameMode.cy.ts
+++ cypress/e2e/gameMode.cy.ts
@@ -1,4 +1,4 @@
-describe("My First Test", () => {
+describe("Testing if game modes are properly added", () => {
   it("Should show the 12 card game mode", () => {
     cy.visit("/");
 
@@ -16,5 +16,89 @@ describe("My First Test", () => {
     cy.get("[data-testid=20-card-mode]").click();
 
     cy.url().should("include", "bomb?gm=20");
+  });
+  it("12 cards game mode should finish with 3 moves", () => {
+    cy.visit("/bomb?gm=12");
+
+    cy.wait(500);
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
+
+    let moves = 3;
+    cy.get("[data-testid=cards-container]>button").each((element) => {
+      if (moves === 0) {
+        return false;
+      }
+      if (element.find("[data-testid='gold']").length > 0) {
+        moves--;
+
+        cy.wrap(element).click();
+      }
+    });
+
+    cy.get("[data-testid=end-modal").should("be.visible");
+  });
+  it("20 cards game mode should finish with 5 moves", () => {
+    cy.visit("/bomb?gm=20");
+
+    cy.wait(500);
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
+
+    let moves = 5;
+    cy.get("[data-testid=cards-container]>button").each((element) => {
+      if (moves === 0) {
+        return false;
+      }
+      if (element.find("[data-testid='gold']").length > 0) {
+        moves--;
+
+        cy.wrap(element).click();
+      }
+    });
+
+    cy.get("[data-testid=end-modal").should("be.visible");
+  });
+  it("12 cards game mode should not finish if 3 moves aren't made yet", () => {
+    cy.visit("/bomb?gm=12");
+
+    cy.wait(500);
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
+
+    let moves = 2;
+    cy.get("[data-testid=cards-container]>button").each((element) => {
+      if (moves === 0) {
+        return false;
+      }
+      if (element.find("[data-testid='gold']").length > 0) {
+        moves--;
+
+        cy.wrap(element).click();
+      }
+    });
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
+  });
+  it("20 cards game mode should not finish if 5 moves aren't made yet", () => {
+    cy.visit("/bomb?gm=12");
+
+    cy.wait(500);
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
+
+    let moves = 4;
+    cy.get("[data-testid=cards-container]>button").each((element) => {
+      if (moves === 0) {
+        return false;
+      }
+      if (element.find("[data-testid='gold']").length > 0) {
+        moves--;
+
+        cy.wrap(element).click();
+      }
+    });
+
+    cy.get("[data-testid=end-modal").should("not.be.visible");
   });
 });
